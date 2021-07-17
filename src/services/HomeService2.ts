f6395745dc1e38e0842cf1ed1f228a417723145e
hillary Ibeanu <ibeanuhillary@gmail.com>
2021-07-18 00:15:14 +0100

    removed unused file

--- src/services/HomeService2.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express'
-import HttpStatus from 'http-status-codes'
-
-export const index = function (req: Request, res: Response) {
-  res.status(HttpStatus.OK).json({ message: 'application up and running...' })
-}
